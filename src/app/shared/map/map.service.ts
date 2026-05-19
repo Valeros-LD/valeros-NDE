60cb6394800833339e3d767bd3df1051e6b657db
Simon Dirks <mail@simondirks.com>
2026-05-19 17:21:15 +0200

    Update map tile layer

--- src/app/shared/map/map.service.ts
+++ src/app/shared/map/map.service.ts
@@ -64,14 +64,13 @@ export class MapService {
     const map = L.map(container).setView(center, zoom);
 
     L.tileLayer(
-      'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}',
+      'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
       {
-        minZoom: 0,
-        maxZoom: 20,
         attribution:
-          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: 'png',
-      } as any,
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20,
+      },
     ).addTo(map);
 
     return map;
