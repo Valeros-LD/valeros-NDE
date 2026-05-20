6b7f7a4cf4ee563c9e9055d48cb12cb80b55e745
Simon Dirks <mail@simondirks.com>
2026-05-20 15:20:18 +0200

    Save config to session storage

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, signal, computed } from '@angular/core';
+import { Injectable, signal, computed, effect } from '@angular/core';
 import { WidgetsSettings } from '../shared/widgets/types/widget-config';
 import { ViewType } from '../features/search/views/types/view-type';
 import { FacetConfig } from './facets.config';
+import { ViewsSettings } from '../features/search/views/types/view-config';
 
 export interface AppConfig {
   facets: FacetConfig[];
@@ -14,23 +15,49 @@ export interface AppConfig {
       map: WidgetsSettings;
     };
   };
-  views: {
-    defaultView: ViewType;
-  };
+  views: ViewsSettings;
 }
 
 @Injectable({ providedIn: 'root' })
 export class ConfigService {
+  private readonly STORAGE_KEY = 'valerosConfig';
   private config = signal<AppConfig | null>(null);
 
   readonly facets = computed(() => this.config()?.facets ?? []);
   readonly widgets = computed(() => this.config()?.widgets);
+  readonly views = computed(() => this.config()?.views);
   readonly defaultView = computed(
-    () => this.config()?.views.defaultView ?? 'list',
+    () => this.config()?.views?.defaultView ?? 'list',
   );
 
+  constructor() {
+    this.initSaveConfigToSessionStorageOnChange();
+  }
+
+  initSaveConfigToSessionStorageOnChange() {
+    effect(() => {
+      const config = this.config();
+      if (config) {
+        sessionStorage.setItem(this.STORAGE_KEY, JSON.stringify(config));
+      }
+    });
+  }
+
   initialize(config: AppConfig): void {
-    this.config.set(config);
+    // TODO: Validate config structure
+    const savedConfig = this.loadConfigFromSessionStorage();
+    this.config.set(savedConfig ?? config);
+  }
+
+  private loadConfigFromSessionStorage(): AppConfig | null {
+    const stored = sessionStorage.getItem(this.STORAGE_KEY);
+    if (!stored) return null;
+
+    try {
+      return JSON.parse(stored);
+    } catch {
+      return null;
+    }
   }
 
   updateConfig(updates: Partial<AppConfig>): void {
