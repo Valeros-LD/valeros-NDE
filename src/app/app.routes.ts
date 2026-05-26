157df5d9cd578194695b8bbdf103f36a3ce7d436
Simon Dirks <mail@simondirks.com>
2026-05-26 12:53:46 +0200

    Add 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Route } from '@angular/router';
 import { DetailsComponent } from './features/details/components/details-page/details.component';
 import { SearchPageComponent } from './features/search/components/search-page/search-page.component';
 import { ConfigPageComponent } from './features/config/config-page.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { detailsRouteMatcher } from './details-route.matcher';
 
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: SearchPageComponent,
+  },
+  {
+    path: 'search',
+    component: SearchPageComponent,
+  },
   {
     path: 'config',
     component: ConfigPageComponent,
@@ -15,6 +24,6 @@ export const appRoutes: Route[] = [
   },
   {
     path: '**',
-    component: SearchPageComponent,
+    component: NotFoundComponent,
   },
 ];
