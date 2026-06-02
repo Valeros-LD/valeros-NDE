f41c45a55b922fcd30fea3a21bc0e65eef69cae1
Simon Dirks <mail@simondirks.com>
2026-06-02 13:15:58 +0200

    Add warning alert to config page

--- src/app/config/config-page/config-page.component.ts
+++ src/app/config/config-page/config-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherAlertTriangle } from '@ng-icons/feather-icons';
 import { FacetsConfig } from './facets-config/facets-config.component';
 import { ViewsConfigComponent } from './views-config/views-config.component';
 
 @Component({
   selector: 'app-config-page',
-  imports: [FacetsConfig, ViewsConfigComponent],
+  imports: [FacetsConfig, ViewsConfigComponent, NgIconComponent],
   templateUrl: './config-page.component.html',
+  viewProviders: [provideIcons({ featherAlertTriangle })],
 })
 export class ConfigPageComponent {}
